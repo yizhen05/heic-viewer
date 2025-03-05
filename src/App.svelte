9f308fa2ea0babe50cd0680b013020fd1c552650
yizhen05 <usukumonez@gmail.com>
2025-03-05 16:05:36 +0900

    どうやって表示すんだよ!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/App.svelte
+++ src/App.svelte
@@ -1,52 +1,16 @@
-<script>
-	import heic2any from "heic2any";
-
-	let file;
-	let imageUrl = "";
-	let convertedBlob = null;
-	let isFileSelected = false; // ファイルが選択されたかどうか
-
-	async function handleFile(event) {
-		const selectedFile = event.target.files[0];
-		if (!selectedFile) return;
-
-		isFileSelected = true; // ファイル選択時にボタンを有効化
-
-		try {
-			convertedBlob = await heic2any({
-				blob: selectedFile,
-				toType: "image/png",
-			});
-
-			// 変換結果が Blob 形式なので、URL を生成
-			imageUrl = URL.createObjectURL(convertedBlob);
-		} catch (error) {
-			console.error("HEIC変換エラー:", error);
-			convertedBlob = null;
-			imageUrl = "";
-		}
-	}
-</script>
-
+<h1>HEIC VIEWER</h1>
 <main>
-	<input type="file" accept=".heic" on:change={handleFile} />
-	<button on:click={() => {}} disabled={!isFileSelected}>表示</button>
-
-	{#if imageUrl}
-		<img
-			src={imageUrl}
-			alt="変換された HEIC ファイル"
-			style="max-width: 100%;"
-		/>
-	{/if}
-</main>
-
-<!-- <script>
-</script>
-
-<main>
-	<input type="file" name="example" accept=".heic" />
+	<input type="file" accept=".heic" class="loading_heic" />
 </main>
 
 <style>
-</style> -->
+	h1 {
+		text-align: center;
+	}
+	main {
+		text-align: center;
+	}
+	.loading_heic {
+		margin: 50px auto;
+	}
+</style>
